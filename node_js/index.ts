d400c92fec242b94a3740406a2747f83a4496cd8
sudarshan161219 <godhustler90956@gmail.com>
2025-04-15 08:03:00 +0530

    node-js

---
+++ node_js/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response, Express } from "express";
+
+const app: Express = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
