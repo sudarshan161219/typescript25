81c93ec6ccf8c3b9c4a061914d6ef902faa3efe4
sudarshan161219 <godhustler90956@gmail.com>
2025-04-26 10:56:18 +0530

    ts-react

---
+++ ts-react/src/main.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {router} from "./routes.tsx"
+import { RouterProvider } from 'react-router'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
---
+++ ts-react/src/pages/tasks.tsx
@@ -0,0 +1,5 @@
+import { FC, ReactElement } from "react";
+
+export const Tasks: FC = (): ReactElement => {
+  return <h1>Tasks Page</h1>;
+};
